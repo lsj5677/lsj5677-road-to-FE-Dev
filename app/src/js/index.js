eca1649c873ee1eca21fdb400d450762154ac33e
lsj5677 <46502337+lsj5677@users.noreply.github.com>
2020-09-18 19:40:35 +0900

    Modify header

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,5 +1,35 @@
 import $ from 'jquery';
 
-console.log($('h1'));
+(() => {
+  const menuIcon = document.querySelector('.header-menu-icon');
+  const contactIcon = document.querySelector('.header-contact-icon');
+  const CLOSE = 'close-button';
 
-console.log(1);
+  function NavOn() {
+    const headerNavigation = document.querySelector('.header-navigation');
+    const menuIconLine = document.querySelectorAll('.menu-icon-line');
+
+    headerNavigation.classList.toggle('nav-on');
+    menuIconLine.forEach((line) => line.classList.toggle(CLOSE));
+  }
+
+  function contactOn() {
+    const headerNavigation = document.querySelector('.header-contact-items');
+
+    headerNavigation.classList.toggle('contact-on');
+    contactIcon.classList.toggle(CLOSE);
+
+    const hasCloseButton = contactIcon.classList.contains(CLOSE);
+
+    if (!hasCloseButton) {
+      document.body.style.backgroundColor = 'white';
+    } else {
+      document.body.style.backgroundColor = '#D7E9EE';
+    }
+
+  }
+
+  menuIcon.addEventListener('click', NavOn);
+  contactIcon.addEventListener('click', contactOn);
+
+})();
