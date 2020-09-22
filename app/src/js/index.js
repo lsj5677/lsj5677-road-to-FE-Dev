656151b1143f7b798b9e8079059c272cf5ed0fe3
lsj5677 <46502337+lsj5677@users.noreply.github.com>
2020-09-22 17:55:48 +0900

    Create main

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,5 +1,34 @@
-import $ from 'jquery';
+(() => {
 
-console.log($('h1'));
+  const debounce = (func, wait = 20, immediate = true) => {
+    let timeout;
 
-console.log(1);
+    return function () {
+
+      const context = this;
+      const args = arguments;
+      const later = () => {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      }
+
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+
+    }
+  }
+
+  const mainConItem = document.querySelectorAll('.main-contents-items');
+
+  function mainContentsEffect() {
+    let pageY = pageYOffset / 6;
+    mainConItem.forEach((item) => {
+      item.style.transform = `translateY(-${pageY}px)`;
+      item.style.transition = `transform .5s ease`;
+    });
+  }
+
+  document.addEventListener('scroll', mainContentsEffect);
+})();
