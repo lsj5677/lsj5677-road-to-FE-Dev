4c4bf0d37a4c1cabc07c787ca5f08c76bde1fc98
lsj5677 <46502337+lsj5677@users.noreply.github.com>
2020-09-22 18:52:25 +0900

    Add main

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -36,26 +36,6 @@
 
   }
 
-  const debounce = (func, wait = 20, immediate = true) => {
-    let timeout;
-
-    return function () {
-
-      const context = this;
-      const args = arguments;
-      const later = () => {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      }
-
-      const callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
-
-    }
-  }
-
   const mainConItem = document.querySelectorAll('.main-contents-items');
 
   function mainContentsEffect() {
