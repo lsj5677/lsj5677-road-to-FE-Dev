a29bd52bf1f1dd8ff1eee2ca2173874cf146f87b
lsj5677 <46502337+lsj5677@users.noreply.github.com>
2020-09-23 20:11:28 +0900

    Add main

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,14 +1,54 @@
 (() => {
-  const mainContainer = document.querySelector('.main-container');
+  const header = document.querySelector('.header');
+  const menuIcon = document.querySelector('.header-menu-icon');
+  const contactIcon = document.querySelector('.header-contact-icon');
+  const CLOSE = 'close-button';
 
-  function mainContainerScroll() {
+  function NavOn() {
+    const headerNavigation = document.querySelector('.header-navigation');
+    const menuIconLine = document.querySelectorAll('.menu-icon-line');
+
+    headerNavigation.classList.toggle('nav-on');
+    menuIconLine.forEach((line) => line.classList.toggle(CLOSE));
+    const hasCloseButton = headerNavigation.classList.contains('nav-on');
+
+    if (!hasCloseButton) {
+      header.style.zIndex = '0';
+    } else {
+      header.style.zIndex = '1';
+    }
+  }
+
+  function contactOn() {
+    const headerNavigation = document.querySelector('.header-contact-items');
+
+    headerNavigation.classList.toggle('contact-on');
+    contactIcon.classList.toggle(CLOSE);
+    const hasCloseButton = contactIcon.classList.contains(CLOSE);
+
+    if (!hasCloseButton) {
+      header.style.background = 'none';
+      header.style.zIndex = '0';
+    } else {
+      header.style.background = '#D7E9EE';
+      header.style.zIndex = '1';
+    }
+
+  }
+
+  const mainConItem = document.querySelectorAll('.main-contents-items');
+
+  function mainContentsEffect() {
     let pageY = pageYOffset / 6;
-
-    mainContainer.style.transform = `translateY(-${pageY}px)`;
-    mainContainer.style.transition = `transform .8s ease`;
+    mainConItem.forEach((item) => {
+      item.style.transform = `translateY(-${pageY}px)`;
+      item.style.transition = `transform .5s ease`;
+    });
   }
 
 
-  document.addEventListener('scroll', mainContainerScroll);
+  menuIcon.addEventListener('click', NavOn);
+  contactIcon.addEventListener('click', contactOn);
+  document.addEventListener('scroll', mainContentsEffect);
 
 })();
