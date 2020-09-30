b4c2c69c7d1e26133c1b793ff787ac204c46d673
lsj5677 <46502337+lsj5677@users.noreply.github.com>
2020-09-30 21:44:18 +0900

    Create main

---
+++ app/src/js/index.js
@@ -0,0 +1,35 @@
+const introTitle = document.querySelector(".intro-title h1");
+introTitle.innerHTML = introTitle.textContent.replace(
+  /([^\x00-\x80]|\w)/g,
+  "<span class='letter'>$&</span>"
+);
+
+anime
+  .timeline({ loop: false })
+  .add({
+    targets: ".letter",
+    translateX: [100, 0],
+    translateZ: 0,
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    duration: 800,
+    delay: function (e, i) {
+      return 200 + 50 * i;
+    }
+  })
+  .add({
+    targets: ".letter",
+    translateX: [0, -100],
+    opacity: [1, 0],
+    easing: "easeInExpo",
+    duration: 500,
+    delay: function (e, i) {
+      return 200 + 50 * i;
+    }
+  });
+
+TweenMax.to(".intro", 2, {
+  delay: 2,
+  top: "-100%",
+  ease: Expo.easeInOut
+});
