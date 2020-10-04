ed18d96636a67b6b99276225cc0bf7852f8c5340
lsj5677 <46502337+lsj5677@users.noreply.github.com>
2020-10-04 20:23:36 +0900

    Recreate main

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,47 +1,52 @@
-const introTitle = document.querySelector(".intro-title h1");
-introTitle.innerHTML = introTitle.textContent.replace(
-  /([^\x00-\x80]|\w)/g,
-  "<span class='letter'>$&</span>"
-);
+$(function () {
+  let $cursorX = 0;
+  let $cursorY = 0;
+  let $left = 0;
+  let $top = 0;
 
-anime
-  .timeline({ loop: false })
-  .add({
-    targets: ".letter",
-    translateX: [100, 0],
-    translateZ: 0,
-    opacity: [0, 1],
-    easing: "easeOutExpo",
-    duration: 800,
-    delay: function (e, i) {
-      return 200 + 50 * i;
-    }
-  })
-  .add({
-    targets: ".letter",
-    translateX: [0, -100],
-    opacity: [1, 0],
-    easing: "easeInExpo",
-    duration: 500,
-    delay: function (e, i) {
-      return 200 + 50 * i;
-    }
+
+  $(document).mousemove(e => {
+    $cursorX = e.clientX;
+    $cursorY = e.clientY;
+
   });
 
-TweenMax.to(".intro", 2, {
-  delay: 2,
-  top: "-100%",
-  ease: Expo.easeInOut
+  setInterval(() => {
+    $left += ($cursorX - $left) / 12;
+    $top += ($cursorY - $top) / 12;
+    $('.custom-cursor').css({ left: $left + 'px', top: $top + 'px' });
+  }, 30);
+
+  let moreShape = new TimelineMax({ paused: true })
+    .to('.custom-cursor', 0.3, { scale: 2 })
+    .to('.more-shape', 0.3, { opacity: 1 });
+
+  $('.work-img-box').each(function (i, ele) {
+    let workImgHoverTI = new TimelineMax({ paused: true });
+    workImgHoverTI.to($(this).find('img'), 0.3, { opacity: 0.3 })
+      .to($(this).find('span'), 0.3, { opacity: 1, y: '50px' });
+    ele.animation = workImgHoverTI;
+  });
+
+  $('.work-img-box').hover(
+    function () {
+      this.animation.play();
+      moreShape.play();
+    },
+    function () {
+      this.animation.reverse();
+      moreShape.reverse();
+    }
+  );
+
 });
 
+  // function scrollEffect() {
+  //   const mainContainer = document.querySelector('.main-container');
+  //   const pageY = pageYOffset / 6;
+  //   console.log(1);
+  //   mainContainer.style.transform = `translateY(-${pageY}px)`;
+  //   mainContainer.style.transition = `transform .8s ease`;
+  // }
 
-
-function scrollEffect() {
-  const mainContainer = document.querySelector('.main-container');
-  const pageY = pageYOffset / 6;
-  console.log(1);
-  mainContainer.style.transform = `translateY(-${pageY}px)`;
-  mainContainer.style.transition = `transform .8s ease`;
-}
-
-document.addEventListener('scroll', scrollEffect);
+  // document.addEventListener('scroll', scrollEffect);
