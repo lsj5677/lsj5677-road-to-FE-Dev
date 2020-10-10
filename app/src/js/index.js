077b23a4797ff0d799ed639f0bc5f942cfc463ba
lsj5677 <46502337+lsj5677@users.noreply.github.com>
2020-10-11 01:49:16 +0900

    Modify webpack and index js

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,111 +1,151 @@
+import $ from 'jquery';
+import anime from 'animejs/lib/anime.es.js';
+import gsap from "gsap";
+
 $(function () {
-  let $cursorX = 0;
-  let $cursorY = 0;
-  let $left = 0;
-  let $top = 0;
+
+  handleCursor();
+  function handleCursor() {
+    let $cursorX = 0;
+    let $cursorY = 0;
+    let $left = 0;
+    let $top = 0;
 
 
-  $(document).mousemove(e => {
-    $cursorX = e.clientX;
-    $cursorY = e.clientY;
+    $(document).mousemove(e => {
+      $cursorX = e.clientX;
+      $cursorY = e.clientY;
 
-  });
-
-  setInterval(() => {
-    $left += ($cursorX - $left) / 12;
-    $top += ($cursorY - $top) / 12;
-    $('.custom-cursor').css({ left: $left + 'px', top: $top + 'px' });
-  }, 10);
-
-  let moreCursorShape = new TimelineMax({ paused: true })
-    .to('.custom-cursor', 0.3, { scale: 2 })
-    .to('.more-shape', 0.3, { opacity: 1 });
-
-  let menuLinkCursorShape = new TimelineMax({ paused: true })
-    .to('.custom-cursor', 0.3, { scale: 0.4, backgroundColor: '#fff' });
-
-  $('.btn-icon, .footer-item').each(function (i, ele) {
-    let btnIconTl = new TimelineMax({ paused: true });
-    btnIconTl.to($(this), 0.3, { opacity: 0.6 });
-    ele.animation = btnIconTl;
-  });
-
-  $('.btn-icon, .footer-item').hover(
-    function () {
-      this.animation.play();
-      menuLinkCursorShape.play();
-    },
-    function () {
-      this.animation.reverse();
-      menuLinkCursorShape.reverse();
-    }
-  );
-
-  $('.work-img-box').each(function (i, ele) {
-    let workImgHoverTl = new TimelineMax({ paused: true });
-    workImgHoverTl.to($(this), 0.3, { opacity: 0.5 })
-      .to($(this).find('span'), 0.3, { opacity: 1 });
-    ele.animation = workImgHoverTl;
-  });
-
-  $('.work-img-box').hover(
-    function () {
-      this.animation.play();
-      moreCursorShape.play();
-    },
-    function () {
-      this.animation.reverse();
-      moreCursorShape.reverse();
-    }
-  );
-
-  $('.intro h4').html(
-    $('.intro h4').html()
-      .replace(/./g, '<span>$&</span>')
-      .replace(/\s/g, '&nbsp;')
-  );
-
-  anime
-    .timeline({ loop: false })
-    .add({
-      targets: ".intro h4 span",
-      translateX: [-100, 0],
-      translateZ: 0,
-      opacity: [0, 1],
-      easing: "easeOutExpo",
-      duration: 300,
-      delay: function (el, i) {
-        return 200 + 50 * i;
-      }
-    })
-    .add({
-      targets: ".intro h4 span",
-      translateX: [0, 100],
-      opacity: [1, 0],
-      easing: "easeInExpo",
-      duration: 500,
-      delay: function (el, i) {
-        return 200 + 50 * i;
-      }
     });
 
+    setInterval(() => {
+      $left += ($cursorX - $left) / 12;
+      $top += ($cursorY - $top) / 12;
+      $('.custom-cursor').css({ left: $left + 'px', top: $top + 'px' });
+    }, 10);
 
-  TweenMax.to('.intro', 0.6, {
-    delay: 2.5,
-    top: '-100vh',
-    ease: Expo.easeInOut
-  });
+    let moreCursorShape = new TimelineMax({ paused: true })
+      .to('.custom-cursor', 0.3, { scale: 2 })
+      .to('.more-shape', 0.3, { opacity: 1 });
+
+    let menuLinkCursorShape = new TimelineMax({ paused: true })
+      .to('.custom-cursor', 0.3, { scale: 0.4, backgroundColor: '#fff' });
+
+
+    $('.btn-icon, .footer-item').each(function (i, ele) {
+      let btnIconTl = new TimelineMax({ paused: true });
+      btnIconTl.to($(this), 0.3, { opacity: 0.6 });
+      ele.animation = btnIconTl;
+    });
+
+    $('.btn-icon, .footer-item').hover(
+      function () {
+        this.animation.play();
+        menuLinkCursorShape.play();
+      },
+      function () {
+        this.animation.reverse();
+        menuLinkCursorShape.reverse();
+      }
+    );
+
+    $('.work-img-box').each(function (i, ele) {
+      let workImgHoverTl = new TimelineMax({ paused: true });
+      workImgHoverTl.to($(this), 0.3, { opacity: 0.5 })
+        .to($(this).find('span'), 0.3, { opacity: 1 });
+      ele.animation = workImgHoverTl;
+    });
+
+    $('.work-img-box').hover(
+      function () {
+        this.animation.play();
+        moreCursorShape.play();
+      },
+      function () {
+        this.animation.reverse();
+        moreCursorShape.reverse();
+      }
+    );
+  }
+
+  const splitText = (text) => {
+    return text.split('').map(char => `<span>${char}</span>`).join('');
+  }
+
+  $('.intro h4').html(
+    splitText($('.intro h4').text())
+  );
+
+  // 처음 실행되는 애들
+  init();
+
+  function init() {
+    // 여기서 함수실행
+    initElements();
+    fadeInOutIntroText();
+    slideUpBackground();
+  }
+
+  // footer면 footer section별
+
+  function fadeInOutIntroText() {
+    anime
+      .timeline({ loop: false })
+      .add({
+        targets: ".intro h4 span",
+        translateX: [-100, 0],
+        translateZ: 0,
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 300,
+        delay: function (el, i) {
+          return 200 + 50 * i;
+        }
+      })
+      .add({
+        targets: ".intro h4 span",
+        translateX: [0, 100],
+        opacity: [1, 0],
+        easing: "easeInExpo",
+        duration: 500,
+        delay: function (el, i) {
+          return 200 + 50 * i;
+        }
+      });
+  }
+
+
+  function slideUpBackground() {
+    TweenMax.to('.intro', 0.6, {
+      delay: 2.5,
+      top: '-100vh',
+      ease: Expo.easeInOut
+    });
+  }
+
 
 });
 
-const headerNavigation = document.querySelector('.header-navigation');
-const headerContact = document.querySelector('.header-contact');
-const navIconBtn = document.querySelector('.nav-icon-btn');
-const contactIconBtn = document.querySelector('.contact-icon-btn');
-const CLOSE = 'close-btn';
-const mainContainer = document.querySelector('.main-container');
-const titleItems = document.querySelectorAll('.title-item');
-const scrollImgContents = document.querySelectorAll('.work-img-box a');
+let headerNavigation;
+let headerContact;
+let navIconBtn;
+let contactIconBtn;
+let mainContainer;
+let titleItems;
+let scrollImgContents;
+let CLOSE = 'close-btn';
+
+initElements();
+
+function initElements() {
+  headerNavigation = $('.header-navigation');
+  headerContact = $('.header-contact');
+  navIconBtn = $('.nav-icon-btn');
+  contactIconBtn = $('.contact-icon-btn');
+  mainContainer = $('.main-container');
+  titleItems = $('.title-item');
+  scrollImgContents = $('.work-img-box a');
+}
 
 function navOn() {
   headerNavigation.classList.toggle('nav-on');
