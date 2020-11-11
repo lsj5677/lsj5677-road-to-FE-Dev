f651e893b8de16ffb125da89cdd147a43c6627de
lsj5677 <46502337+lsj5677@users.noreply.github.com>
2020-11-12 02:48:15 +0900

    Create Landingpage Content

--- app/src/js/landing.js
+++ app/src/js/landing.js
@@ -1,2 +1,43 @@
 import './common';
 import $ from 'jquery';
+
+const screenSize = {
+  large: 1024,
+  tablet: 768,
+  small: 575,
+};
+const scrollMax = 95;
+
+let headerEl;
+let headerMoBtnEl;
+let headerNavEl;
+
+init();
+function init() {
+  initEl();
+  scrollHeader();
+  showMoHeader();
+}
+
+function initEl() {
+  headerEl = $('.header');
+  headerMoBtnEl = $('.header-mo-btn');
+  headerNavEl = $('.header-nav');
+}
+
+function scrollHeader() {
+  $(window).scroll(() => {
+    if (scrollMax < pageYOffset) {
+      headerEl.addClass('header-bg-on');
+    } else {
+      headerEl.removeClass('header-bg-on');
+    }
+  });
+}
+
+function showMoHeader() {
+  headerMoBtnEl.click(() => {
+    headerMoBtnEl.toggleClass('close');
+    headerNavEl.toggleClass('header-show');
+  });
+}
